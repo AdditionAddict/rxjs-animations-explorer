7fbaef0cf6992172f2e34eb3d75f14a1988449af
Andrew Allen <andrewallen1.6180@yahoo.co.uk>
2023-03-29 02:43:52 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rxjs-animation-explorer-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'rxjs-animation-explorer';
+}
